7b4ff5445594760a5b0ec4d66db02b5b88f188f6
Mwau stephen <stephenmwau41@gmail.com>
2021-12-28 16:22:15 -0800

    finish a click modal program

---
+++ modals/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+// Anonymous function to open model
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// Anonymous function to close modal
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Anonymus function to close modal on clicking the overlay
+const overlayCloseModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Access the modal buttons
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", overlayCloseModal);
